7439c60e6d7408df06e2f55b496a63ef24960a2a
rafgargal <rafgargal@alum.us.es>
2023-11-23 12:21:26 +0100

    test: password reset email sent successfully

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -179,7 +180,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 STATIC_URL = '/static/'
 
-# Para el desarrollo verlo en consola:
+# Para el desarrollo (verlo en consola):
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Backend de Gmail en lugar del de la consola
