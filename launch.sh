4451727659683cabd1496c38629a49113a46562c
jualeomad <jualeomad@alum.us.es>
2023-12-17 20:51:58 +0100

    fix: cd

--- launch.sh
+++ launch.sh
@@ -8,6 +8,5 @@ rm -rf authentication/migrations/__pycache__/
 ./manage.py collectstatic --noinput
 ./manage.py makemigrations
 ./manage.py migrate
-./manage.py createsuperuser --noinput
 # gunicorn -w 5 decide.wsgi:application --timeout=500
 ./manage.py runserver
